04230241f81cf3d093fc5583886bd4d477fe4612
rcabreros <rcabreros@users.noreply.github.com>
2018-12-30 21:33:47 +0800

    working copy
    working copy

--- test1/test1_spec.js
+++ test1/test1_spec.js
@@ -1,15 +1,20 @@
 describe('test 1', function() {
   it('from google to youtube', function() {
-    browser.get('https://www.google.com/');
+    browser.waitForAngularEnabled(false);
+    browser.manage().window().maximize();
+    browser.get('https://www.google.com', 15000);
 
-    element(by.id('gbwa')).click();
+    var EC = protractor.ExpectedConditions;
+    var elem1 = $('a.gb_b.gb_qc');
+    //browser.wait(EC.elementToBeSelected(elem1), 15000, 'Element taking too long to appear in the DOM');
+    elem1.click();
+
 
     var until = protractor.ExpectedConditions;
     var elem = element(by.id('gb36'));
     browser.wait(until.presenceOf(elem), 15000, 'Element taking too long to appear in the DOM');
     elem.click();
-
-    var EC = protractor.ExpectedConditions;
+    
     browser.wait(EC.titleIs('YouTube'), 15000);
     browser.wait(EC.urlContains('youtube'), 15000);
 
