18fa7102630f84b237948141885687723d516680
rcabreros <rcabreros@users.noreply.github.com>
2018-12-31 00:01:10 +0800

    working test 2 solution
    working test 2 solution

--- test 2/test2_spec.js
+++ test 2/test2_spec.js
@@ -1,23 +1,28 @@
 describe('test 2', function() {
   it('go to youtube and click the first video', function() {
-    browser.get('https://www.youtube.com/');
+    browser.waitForAngularEnabled(false);
+    browser.manage().window().maximize();
+    browser.get('https://www.youtube.com', 15000);
 
     var first_vid = element(by.xpath('//*[@id="title-container" and contains(.,"Trending")]/parent::div/following-sibling::div//*[@id="items"]/ytd-grid-video-renderer[1]//a[@id="thumbnail"]'));
     first_vid.click();
   });
-  it('click the first video under UP NEXT', function() {    
+  it('click the first video under UP NEXT', function() {
     
-    var EC = protractor.ExpectedConditions;
-    browser.wait(EC.elementToBeSelected($('#items')), 5000);
-    element(by.xpath('//*[@id="items"]/ytd-compact-autoplay-renderer//a[@id="thumbnail"]')).click();    
+    var elem1 = element(by.xpath('//div[@id="upnext" and contains(.,"Up next")]/parent::div/following-sibling::div'));
+    browser.sleep(5000);
+    elem1.click();  
   });
   it('go back to youtube and verify', function() {
 
-    element(by.id('logo-icon-container')).click();
+    var EC = protractor.ExpectedConditions;
+    var elem2 = element(by.xpath('//*[@id="logo"]//*[@id="logo" and @title="YouTube Home"]'));
+    elem2.click();
+    
     browser.wait(EC.titleIs('YouTube'), 15000);
     browser.wait(EC.urlContains('youtube'), 15000);
 
     expect(browser.getTitle()).toEqual('YouTube');
-    expect(browser.getCurrentUrl()).toEqual('https://www.youtube.com/?gl=PH');
+    expect(browser.getCurrentUrl()).toEqual('https://www.youtube.com/');
   });
 });
