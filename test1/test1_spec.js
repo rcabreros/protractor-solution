73a7a1fdac6ccba9b410454a8d7bac9c3b563e09
rcabreros <rcabreros@users.noreply.github.com>
2019-01-28 23:54:11 +0800

    making it look clean
    making it look clean

--- test1/test1_spec.js
+++ test1/test1_spec.js
@@ -1,22 +1,26 @@
 describe('test 1', function() {
-  it('from google to youtube', function() {
+
+  var until = protractor.ExpectedConditions;
+
+  beforeAll(function() {
+
     browser.waitForAngularEnabled(false);
     browser.manage().window().maximize();
     browser.get('https://www.google.com', 15000);
+  });
 
-    var EC = protractor.ExpectedConditions;
-    var elem1 = $('a.gb_b.gb_qc');
-    browser.wait(EC.presenceOf(elem1), 15000, 'Element taking too long to appear in the DOM');
+  it('from google to youtube', function() {    
+    
+    var elem1 = $('#gbwa');
+    browser.wait(until.presenceOf(elem1), 15000, 'Element taking too long to appear in the DOM');
     elem1.click();
 
-
-    var until = protractor.ExpectedConditions;
-    var elem = element(by.id('gb36'));
+    var elem = $('#gb36');
     browser.wait(until.presenceOf(elem), 15000, 'Element taking too long to appear in the DOM');
     elem.click();
     
-    browser.wait(EC.titleIs('YouTube'), 15000);
-    browser.wait(EC.urlContains('youtube'), 15000);
+    browser.wait(until.titleIs('YouTube'), 15000);
+    browser.wait(until.urlContains('youtube'), 15000);
 
     expect(browser.getTitle()).toEqual('YouTube');
     expect(browser.getCurrentUrl()).toEqual('https://www.youtube.com/?gl=PH');
--- test1/test_conf.js
+++ test1/test_conf.js
@@ -4,7 +4,7 @@ exports.config = {
 
   // Capabilities to be passed to the webdriver instance.
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'firefox'
   },
 
   // Framework to use. Jasmine is recommended.
